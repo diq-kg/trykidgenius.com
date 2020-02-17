cfdbfffbf71c17c8bdd028d45a7f6e712a46ae60
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2020-02-16 22:33:51 -0500

    finish responsive navbar

--- src/components/header.js
+++ src/components/header.js
@@ -5,8 +5,99 @@ import React, { useState } from 'react';
 function Header({ siteTitle }) {
   const [isExpanded, toggleExpansion] = useState(false);
 
+  function menuButton(show) {
+    if (show) {
+      return (
+        <path
+          fill-rule="evenodd"
+          d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+        />
+      );
+    } else {
+      return (
+        <path
+          fill-rule="evenodd"
+          d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+        />
+      );
+    }
+  }
+
   return (
-    <nav className="h-20 p-4 text-gray-brand bg-gray-700">
+    <header
+      class={`${
+        isExpanded ? `` : `h-20`
+      } font-brand md:flex md:justify-between md:items-center md:pr-3`}
+    >
+      <div className="flex h-20 items-center justify-between px-4 py-3 md:p0">
+        <div className="flex items-center">
+          <div className="w-8 h-8 rounded-lg bg-brand-primary"></div>
+          <span className="text-3xl font-bold ml-2 font-title">KidGenius</span>
+        </div>
+        <div className="md:hidden">
+          <button
+            className="block text-gray-brand hover:text-gray-700 focus:text-gray-700 focus:outline-none"
+            onClick={() => toggleExpansion(!isExpanded)}
+          >
+            <svg className="h-8 w-8 fill-current" viewBox="0 0 24 24">
+              <title>Menu</title>
+              {menuButton(isExpanded)}
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div
+        class={`${
+          isExpanded ? 'block' : 'hidden'
+        } px-2 pt-4 pb-4 text-gray-brand md:flex md:p0 md-h-auto`}
+      >
+        <a
+          className="block font-semibold hover:bg-gray-300 rounded px-2 py-1 md:p0 md:px-4 md:pt-3"
+          href="#"
+        >
+          Features
+        </a>
+        <a
+          className="block font-semibold hover:bg-gray-300 rounded px-2 py-1 mt-1 md:mt-0 md:p0 md:px-4 md:ml-2 md:pt-3"
+          href="#"
+        >
+          Pricing
+        </a>
+        <a
+          className="block font-semibold hover:bg-gray-300 rounded px-2 py-1 mt-1 md:mt-0 md:ml-2 md:p0 md:px-4 md:pt-3"
+          href="#"
+        >
+          Resources
+        </a>
+        <a
+          className="block font-semibold hover:bg-gray-300 rounded px-2 py-1 mt-1 md:mt-0 md:ml-2 md:p0 md:px-4 md:pt-3"
+          href="#"
+        >
+          FAQ
+        </a>
+        <button className="block w-32 h-12 border-2 bg-white font-semibold hover:bg-gray-300 rounded mt-2 ml-2 md:mt-0 md:ml-10">
+          Login
+        </button>
+        <button className="block w-32 h-12 bg-brand-primary font-semibold hover:bg-gray-300 rounded mt-2 ml-2 md:mt-0 md:ml-4">
+          Sign Up
+        </button>
+      </div>
+    </header>
+  );
+}
+
+Header.propTypes = {
+  siteTitle: PropTypes.string
+};
+
+Header.defaultProps = {
+  siteTitle: ``
+};
+
+export default Header;
+
+{
+  /* <nav className="">
       <div className="flex items-center justify-between">
         <div className="flex items-center">
           <div className="w-8 h-8 rounded-lg bg-brand-primary"></div>
@@ -55,16 +146,5 @@ function Header({ siteTitle }) {
           </Link>
         </div>
       </div>
-    </nav>
-  );
+    </nav> */
 }
-
-Header.propTypes = {
-  siteTitle: PropTypes.string
-};
-
-Header.defaultProps = {
-  siteTitle: ``
-};
-
-export default Header;
