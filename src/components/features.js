5175f482fddb77dd8b89d030e4c2037a5245d8be
Prasanth Vaaheeswaran <prasanth@daycareiq.com>
2020-02-22 16:29:36 -0600

    switching laptops

--- src/components/features.js
+++ src/components/features.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import FeatureCard from './feature-card';
+import Checklist from './feature-icons/checklist';
 
 function Features() {
+  const features = [
+    {
+      title: 'Attendance',
+      desc:
+        'Track attendance and generate monthly reports in one go - available for both kids and staff.',
+      icon: <Checklist />
+    }
+  ];
+
   return (
     <div className="flex border-2 justify-center items-center flex-wrap">
       <h2 className="text-3xl text-center font-extrabold mb-4">
