d8519e03a1b9fad32cfb9bc05fd9ed01743d5cc1
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2020-03-01 15:36:18 -0500

    Add proper quotes and names

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -5,7 +5,7 @@ import dog from '../images/thisisfine.jpg';
 
 function QuoteCard(props) {
   return (
-    <div className="mb-8 flex flex-col lg:mb-0 lg:mx-2 justify-between items-start w-quotex h-quotey px-4 pt-4 pb-6 shadow-lg rounded-md">
+    <div className="mb-8 flex flex-col lg:mb-0 lg:mx-2 justify-evenly items-start w-quotex h-quotey px-4 pt-4 pb-6 shadow-lg rounded-md">
       <p className="text-left text-lg">{props.quote}</p>
       <PersonCard name={props.name} title={props.title} image={props.image} />
     </div>
@@ -17,22 +17,22 @@ function Quotes() {
     {
       quote:
         "It is an excellent way to keep in communication with the teachers and caregivers. I can receive daily reports of my child's school activities through a single channel",
-      title: 'Fine Dog',
-      name: 'Dog',
+      title: 'Parent',
+      name: 'Lucía O',
       image: dog
     },
     {
       quote:
-        "It is an excellent way to keep in communication with the teachers and caregivers. I can receive daily reports of my child's school activities through a single channel",
-      title: 'Fine Dog',
-      name: 'Dog',
+        'The way that the menu and whole site is set up, I like how everything is easy to access and ready to use right away. Easy to train someone to use it as well which is always really helpful.',
+      title: 'Childcare Worker',
+      name: 'Nicole C',
       image: dog
     },
     {
       quote:
-        "It is an excellent way to keep in communication with the teachers and caregivers. I can receive daily reports of my child's school activities through a single channel",
-      title: 'Fine Dog',
-      name: 'Dog',
+        'It is incredibly helpful for administration, especially for Alberta Child Care Claims. It also has helped with communication with families, which is paramount in our line of work.',
+      title: 'Director',
+      name: 'Delaney C',
       image: dog
     }
   ];
