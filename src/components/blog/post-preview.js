63a11d1351511a0460dedc6137cecf6da842a2db
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2020-07-19 23:39:57 -0400

    adjust blog styles

--- src/components/blog/post-preview.js
+++ src/components/blog/post-preview.js
@@ -5,14 +5,14 @@ import { Link } from 'gatsby';
 export default function PostPreview(props) {
   let date = new Date(props.date);
   return (
-    <div className="flex flex-col pb-10">
+    <div className="flex flex-col pb-10 max-w-2xl">
       <div className="pb-1">
         <span className="font-mono">{date.toDateString()}</span>
       </div>
       <Link to={props.url}>
-        <h1 className="font-brand-bold text-2xl">{props.title}</h1>
+        <h1 className="font-brand-bold text-3xl">{props.title}</h1>
       </Link>
-      <div className="pt-2 text-xl">{props.fold}</div>
+      <div className="pt-2 text-lg">{props.fold}</div>
     </div>
   );
 }
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -9,10 +9,10 @@ export default function Template({ data }) {
   const date = new Date(frontmatter.date);
   return (
     <Layout>
-      <div className="flex flex-col pl-3 pt-10">
-        <h1 className="text-4xl font-brand">{frontmatter.title}</h1>
-        <h3 className="font-semibold">{date.toDateString()}</h3>
-        <div className="pt-4" dangerouslySetInnerHTML={{ __html: html }} />
+      <div className="flex flex-col pl-3 pt-10 items-center">
+        <h3 className="font-semibold pb-6">{date.toDateString()}</h3>
+        <h1 className="text-4xl font-brand-bold">{frontmatter.title}</h1>
+        <div className="pt-4 max-w-4xl" dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </Layout>
   );
